ffd7d30621123c1a2b047de4e074c4d58f3b435c
sunhuoxyr <sunhuoxyr@163.com>
2022-12-27 22:44:01 +0800

    修改which-key触发条件

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -29,7 +29,7 @@ local options = {
 
 	triggers_blacklist = {
 		-- list of mode / prefixes that should never be hooked by WhichKey
-		i = { "j", "k" },
+		i = { "<leader>", "j", "k" },
 		v = { "j", "k" },
 	},
 }
